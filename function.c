8c226ce693e76ee39f251445deab80b4251fd6f0
Atul Varma <varmaa@toolness.com>
2009-07-03 19:40:42 -0700

    Moved some code around.

--- function.c
+++ function.c
@@ -53,6 +53,51 @@ dispatchJSFunctionToPython(JSContext *cx,
   return JS_TRUE;
 }
 
+PyTypeObject PYM_JSFunctionType = {
+  PyObject_HEAD_INIT(NULL)
+  0,                           /*ob_size*/
+  "pymonkey.Function",         /*tp_name*/
+  sizeof(PYM_JSFunction),      /*tp_basicsize*/
+  0,                           /*tp_itemsize*/
+  /*tp_dealloc*/
+  (destructor) PYM_JSFunctionDealloc,
+  0,                           /*tp_print*/
+  0,                           /*tp_getattr*/
+  0,                           /*tp_setattr*/
+  0,                           /*tp_compare*/
+  0,                           /*tp_repr*/
+  0,                           /*tp_as_number*/
+  0,                           /*tp_as_sequence*/
+  0,                           /*tp_as_mapping*/
+  0,                           /*tp_hash */
+  0,                           /*tp_call*/
+  0,                           /*tp_str*/
+  0,                           /*tp_getattro*/
+  0,                           /*tp_setattro*/
+  0,                           /*tp_as_buffer*/
+  /*tp_flags*/
+  Py_TPFLAGS_DEFAULT,
+  /* tp_doc */
+  "JavaScript Function.",
+  0,		               /* tp_traverse */
+  0,		               /* tp_clear */
+  0,		               /* tp_richcompare */
+  0,                           /* tp_weaklistoffset */
+  0,		               /* tp_iter */
+  0,		               /* tp_iternext */
+  0,                           /* tp_methods */
+  0,                           /* tp_members */
+  0,                           /* tp_getset */
+  0,                           /* tp_base */
+  0,                           /* tp_dict */
+  0,                           /* tp_descr_get */
+  0,                           /* tp_descr_set */
+  0,                           /* tp_dictoffset */
+  0,                           /* tp_init */
+  0,                           /* tp_alloc */
+  0,                           /* tp_new */
+};
+
 PYM_JSFunction *
 PYM_newJSFunction(PYM_JSContextObject *context,
                   PyObject *callable,
@@ -102,48 +147,3 @@ PYM_newJSFunction(PYM_JSContextObject *context,
 
   return object;
 }
-
-PyTypeObject PYM_JSFunctionType = {
-  PyObject_HEAD_INIT(NULL)
-  0,                           /*ob_size*/
-  "pymonkey.Function",         /*tp_name*/
-  sizeof(PYM_JSFunction),      /*tp_basicsize*/
-  0,                           /*tp_itemsize*/
-  /*tp_dealloc*/
-  (destructor) PYM_JSFunctionDealloc,
-  0,                           /*tp_print*/
-  0,                           /*tp_getattr*/
-  0,                           /*tp_setattr*/
-  0,                           /*tp_compare*/
-  0,                           /*tp_repr*/
-  0,                           /*tp_as_number*/
-  0,                           /*tp_as_sequence*/
-  0,                           /*tp_as_mapping*/
-  0,                           /*tp_hash */
-  0,                           /*tp_call*/
-  0,                           /*tp_str*/
-  0,                           /*tp_getattro*/
-  0,                           /*tp_setattro*/
-  0,                           /*tp_as_buffer*/
-  /*tp_flags*/
-  Py_TPFLAGS_DEFAULT,
-  /* tp_doc */
-  "JavaScript Function.",
-  0,		               /* tp_traverse */
-  0,		               /* tp_clear */
-  0,		               /* tp_richcompare */
-  0,                           /* tp_weaklistoffset */
-  0,		               /* tp_iter */
-  0,		               /* tp_iternext */
-  0,                           /* tp_methods */
-  0,                           /* tp_members */
-  0,                           /* tp_getset */
-  0,                           /* tp_base */
-  0,                           /* tp_dict */
-  0,                           /* tp_descr_get */
-  0,                           /* tp_descr_set */
-  0,                           /* tp_dictoffset */
-  0,                           /* tp_init */
-  0,                           /* tp_alloc */
-  0,                           /* tp_new */
-};
